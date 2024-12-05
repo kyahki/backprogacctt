10288e73c7088344f8ed171bdb20b4f561f030ef
peterdev123 <petersylvanv123@gmail.com>
2024-12-05 12:51:02 +0800

    modified bpnn

--- Backpropagation_Neural_Network_Activity/obj/Debug/net8.0-windows/Backpropagation_Neural_Network_Activity.AssemblyInfo.cs
+++ Backpropagation_Neural_Network_Activity/obj/Debug/net8.0-windows/Backpropagation_Neural_Network_Activity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backpropagation_Neural_Network_Activity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd0d3ea1163d1331336d2145261c227aca4a924")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backpropagation_Neural_Network_Activity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backpropagation_Neural_Network_Activity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
