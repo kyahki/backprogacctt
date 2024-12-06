93af27f2da4e3797c26b07a8c1ad2cd99c8ac3ad
peterdev123 <petersylvanv123@gmail.com>
2024-12-06 14:01:16 +0800

    Modified

--- Backpropagation_Neural_Network_Activity/Form1.Designer.cs
+++ Backpropagation_Neural_Network_Activity/Form1.Designer.cs
@@ -37,21 +37,27 @@ namespace Backpropagation_Neural_Network_Activity
             textBox3 = new TextBox();
             textBox4 = new TextBox();
             textBox5 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox6 = new TextBox();
+            label4 = new Label();
+            textBox7 = new TextBox();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(163, 130);
+            textBox1.Location = new Point(620, 124);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(342, 23);
+            textBox1.Size = new Size(135, 23);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(163, 227);
+            textBox2.Location = new Point(620, 221);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(342, 23);
+            textBox2.Size = new Size(135, 23);
             textBox2.TabIndex = 1;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
@@ -87,32 +93,96 @@ namespace Backpropagation_Neural_Network_Activity
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(163, 314);
+            textBox3.Location = new Point(620, 308);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(342, 23);
+            textBox3.Size = new Size(135, 23);
             textBox3.TabIndex = 2;
             textBox3.TextChanged += textBox3_TextChanged_1;
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(163, 404);
+            textBox4.Location = new Point(620, 398);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(342, 23);
+            textBox4.Size = new Size(135, 23);
             textBox4.TabIndex = 3;
             textBox4.TextChanged += textBox4_TextChanged;
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(698, 268);
+            textBox5.Location = new Point(945, 275);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(342, 23);
+            textBox5.Size = new Size(268, 23);
             textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(646, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(79, 37);
+            label1.TabIndex = 6;
+            label1.Text = "Input";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20F);
+            label2.Location = new Point(1033, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 37);
+            label2.TabIndex = 7;
+            label2.Text = "Output";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.Location = new Point(219, 174);
+            label3.Name = "label3";
+            label3.Size = new Size(194, 28);
+            label3.TabIndex = 8;
+            label3.Text = "No. of Hidden Layers";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(219, 225);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(194, 23);
+            textBox6.TabIndex = 9;
+            textBox6.TextChanged += textBox6_TextChanged_1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.Location = new Point(219, 303);
+            label4.Name = "label4";
+            label4.Size = new Size(207, 28);
+            label4.TabIndex = 10;
+            label4.Text = "No. of Training Epochs";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(219, 359);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(194, 23);
+            textBox7.TabIndex = 11;
+            textBox7.TextChanged += textBox7_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1478, 794);
+            Controls.Add(textBox7);
+            Controls.Add(label4);
+            Controls.Add(textBox6);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(textBox5);
             Controls.Add(textBox4);
             Controls.Add(textBox3);
@@ -152,5 +222,11 @@ namespace Backpropagation_Neural_Network_Activity
         private TextBox textBox3;
         private TextBox textBox4;
         private TextBox textBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox6;
+        private Label label4;
+        private TextBox textBox7;
     }
 }
